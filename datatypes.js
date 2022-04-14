9595e0e536b96dedcc2b55c76d4f186e72aeb3b3
Haykay2020 <yinkadisa@gmail.com>
2022-04-14 14:34:00 +0100

    Assignment one

---
+++ datatypes.js
@@ -0,0 +1,49 @@
+// Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+let myFirstName = 'Abdullah'    
+let myLastName = 'Adisa'      
+let age = 40                
+let isMarried = true      
+let work = null          
+let hobby;
+           
+
+console.log(typeof myFirstName)
+console.log(typeof myLastName)
+console.log(typeof age)
+console.log(typeof isMarried)
+console.log(typeof work)
+console.log(typeof hobby)
+
+
+// Declare four variables without assigning values
+let myLocation;
+let newVariable;
+let designer;
+let sports;
+
+
+// Declare four variables with assigned values
+let myNewLocation = 'Canada';
+let car = 5;
+let gucci = true;
+let club = 'Chelsea FC';
+
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstName = 'Abdullah'
+let lastName = 'Adisa'
+let maritalStatus = 'married'
+let country = 'Nigeria'
+let myAge = 37;
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+let firstName2 = 'Hafsoh', lastName2 = 'Olayiwola', maritalStatus2 = 'single', country2 = 'Canada', myAge2 = 20
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge3 = 25;
+let yourAge = 30;
+
+console.log(`I am ${myAge3} years old.`)
+console.log(`You are ${yourAge} years old.`)
+
---
+++ variable.js
@@ -0,0 +1,9 @@
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+let myFirstName = 'Abdullah'    // string
+let myLastName = 'Adisa'      // string
+let age = 40                 // number
+let isMarried = true       // boolean
+let work = null            // null
+let hobby;                // undefined
+
