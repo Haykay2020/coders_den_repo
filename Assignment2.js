d73b1a8ffb079ed1444789a65ad82d8994206fd0
Haykay2020 <yinkadisa@gmail.com>
2022-05-01 18:47:50 +0100

    assignment 2 updated

--- Assignment2.js
+++ Assignment2.js
@@ -111,9 +111,65 @@ console.log(motherTheraseQoute); /* Love is not patronizing and charity isn't ab
  so don't just give money but reach out your hand instead. */
 
 // Solution 3
-let num = '10'
-console.log(typeof(num)); // string
-let numActual = Number(num);
+let str10 = '10'
+console.log(typeof(str10)); // string
+let numActual = Number(str10);
 console.log(numActual) // 10
 
+
 // Solution 4
+
+
+
+
+// Solution 5
+let str_1 = "python";
+let str_2 = "jargon";
+let str_3 = 'Baygon';
+console.log(str_1.includes("on"), str_2.includes("on"), str_3.includes('on')); // true true true
+
+// Solution 6
+let jargon = 'I hope this course is not full of jargon.'
+console.log(jargon.includes('jargon')); // true
+
+//Solution 7
+let randNum = Math.floor(Math.random() * 101);
+console.log(randNum)
+
+// Solution 8
+let randNum2 = Math.floor(Math.random() * 51);
+console.log(randNum2)
+
+// Solution 9
+let randNum3 = Math.floor(Math.random() * 256);
+console.log(randNum3)
+
+// Solution 10
+let access = 'Javascript';
+let wordAccess = access.length;
+let randWord = Math.floor(Math.random() * wordAccess);
+console.log(access[randWord]);
+
+// Solution 11
+let escapeChar = '1 1 1 1 1 \n2 1 2 4 8 \n3 1 3 9 27 \n4 1 4 16 64 \n5 1 5 25 125';
+console.log(escapeChar) 
+/*
+1 1 1 1 1  
+2 1 2 4 8  
+3 1 3 9 27  
+4 1 4 16 64  
+5 1 5 25 125  */
+
+//Solution 12
+let phrase = 'You cannot end a sentence with because because because is a conjunction';
+let cutPhrase = phrase.substr(30, 25);
+console.log(cutPhrase); //  because because because
+
+
+// EXERCISE 3
+
+// Solution 1
+let lovePhrase = 'Love is the best thing in this world. Some found their love and some are still looking for their love.';
+let wordCount = lovePhrase.match(/(love)/g).length;
+console.log(wordCount); // 2
+
